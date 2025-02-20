38de9ccd32bf949bbb634c82a66b059c321748ee
Eris Lund <38136789+0x5066@users.noreply.github.com>
2025-02-20 13:42:41 +0100

    very basic extraction from skin files

--- main.cpp
+++ main.cpp
@@ -1,7 +1,38 @@
 #include <iostream>
+#include <string>
 #include "tinyxml2/tinyxml2.h"
 
 using namespace tinyxml2;
+
+void parseSkinXML(std::string filepath);
+
+void parseSkinXML(std::string filepath){
+    XMLDocument xml_doc;
+    XMLError eResult = xml_doc.LoadFile(filepath.c_str());
+
+    if (eResult != XML_SUCCESS){
+        std::cout << "File not found, exiting early." << '\n';
+        return;
+    }
+
+    XMLElement* Container = xml_doc.FirstChildElement("container");
+    if (Container != nullptr) std::cout << Container->Name() << '\n';
+    std::cout << Container->Attribute("id") << '\n';
+
+    XMLElement* Layout = Container->FirstChildElement("layout");
+    if (Layout != nullptr) std::cout << Layout->Name() << '\n';
+    std::cout << Layout->Attribute("id") << '\n';
+
+    if (Container == nullptr) {
+        std::cout << "Container not found! Exiting." << '\n';
+        return;
+    }
+
+    if (Layout == nullptr) {
+        std::cout << "Layout not found! Exiting." << '\n';
+        return;
+    }
+}
 
 bool Test()
 {
@@ -13,11 +44,9 @@ bool Test()
     // can i just adjust the root/FirstChildElement willynilly like that?
     // yes
     XMLElement* WAL = xml_doc.FirstChildElement("WinampAbstractionLayer");
-    // if WAL isnt a null pointer, print the name of root
     if (WAL != nullptr) std::cout << WAL->Name() << '\n';
 
     if (WAL == nullptr) {
-        // set WAL to "WasabiXML" if "WinampAbstractionLayer" doesnt exist
         WAL = xml_doc.FirstChildElement("WasabiXML");
         if (WAL != nullptr) std::cout << WAL->Name() << '\n';
     }
@@ -47,7 +76,6 @@ bool Test()
     if (skininfo == nullptr) return false;
 
     // extensively document this
-    // iterate over skininfo, which is what we determined from WAL->FirstChildElement("skininfo");
     for( XMLElement* children_of_skininfo = skininfo->FirstChildElement();
     children_of_skininfo != NULL;
     children_of_skininfo = children_of_skininfo->NextSiblingElement() )
@@ -57,14 +85,13 @@ bool Test()
         std::cout << tagName << ": " << information << '\n';
     }
 
-    // iterate over WAL but only look for the includes
     for( XMLElement* children_of_WAL = WAL->FirstChildElement("include");
     children_of_WAL != NULL;
     children_of_WAL = children_of_WAL->NextSiblingElement("include") )
     {
-        // this is going to be fun
-        const char* fileAttribute = children_of_WAL->Attribute("file");
+        std::string fileAttribute = "skin/" + std::string(children_of_WAL->Attribute("file"));
         std::cout << "include file: " << fileAttribute << '\n';
+        parseSkinXML(fileAttribute);
     }
     return true;
 }
